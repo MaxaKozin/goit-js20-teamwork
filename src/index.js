7869e687881ee21b9dd3623d933afc9cfb5558df
Maxim Kozin <maxa.kozin@gmail.com>
2020-08-11 16:35:00 +0300

    adding apiServices

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import * as apiServices from './services/apiService';
+
 import './style.scss';
+
+// apiServices.fetchRated(1).then(console.log)
+
+// apiServices.fetchById(521034).then(console.log)
