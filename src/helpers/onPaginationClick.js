445fd99f4d7d822d041e52c71f6d12887cdec1e3
Maxim Kozin <maxa.kozin@gmail.com>
2020-08-13 19:59:53 +0300

    fixed pagination bug

--- src/helpers/onPaginationClick.js
+++ src/helpers/onPaginationClick.js
@@ -3,9 +3,10 @@ import refs from '../services/refs';
 import homePageMarkUp from '../views/HomePage';
 
 const onPaginationClick = (page) => {
+  const movieRef = document.querySelector('.movie');
   apiServices
     .fetchRated(page)
-    .then((data) => homePageMarkUp(data, refs.gallery))
+    .then((data) => homePageMarkUp(data, movieRef))
     .catch((error) => console.log(error));
 }
 
