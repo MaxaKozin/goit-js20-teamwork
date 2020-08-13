6aa94b56b03c4f626e6c76dac772d4b5749fa475
Maxim Kozin <maxa.kozin@gmail.com>
2020-08-13 20:06:33 +0300

    fixed form submit

--- src/helpers/onFormSubmit.js
+++ src/helpers/onFormSubmit.js
@@ -4,15 +4,16 @@ import homePageMarkUp from '../views/HomePage';
 import Pagination from '../components/Pagination';
 
 const onFormSubmit = () => {
+  const movieRef = document.querySelector('.movie');
   refs.searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const form = event.currentTarget;
     const inputValue = form.elements.query.value;
     apiServices
       .fetchByQuery(inputValue, 1)
-      .then((data) => homePageMarkUp(data, refs.movie))
+      .then((data) => homePageMarkUp(data, movieRef))
       .catch((error) => console.log(error));
-    refs.gallery.innerHTML = "";
+    movieRef.innerHTML = "";
     Pagination.clear();
     form.reset();
   });
